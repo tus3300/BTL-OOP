8666077abcb7045f141f94115412c8957570a27d
idea <21012098@st.phenikaa-uni.edu.vn>
2023-04-16 16:26:57 +0700

    reup

---
+++ T.Khai/script.js
@@ -0,0 +1,4 @@
+function addItemToCart(id) {
+	// TODO: Add item to cart
+	alert("Item added to cart: " + id);
+}
--- script.js
+++
@@ -1,4 +0,0 @@
-function addItemToCart(id) {
-	// TODO: Add item to cart
-	alert("Item added to cart: " + id);
-}
