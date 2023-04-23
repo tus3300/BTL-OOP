045047f025577bbdd087bf271851334bab3c8c0c
idea <21012098@st.phenikaa-uni.edu.vn>
2023-04-23 23:46:45 +0700

    up file

---
+++ V.Tu/admin/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST",  "local host");
+define("DB_USER",  "root");
+define("DB_PASS",  "");
+define("DB_NAME",  "your_db_name");
+?>
+
---
+++ V.Tu/admin/session.php
@@ -0,0 +1,32 @@
+<?php
+Class Session{
+    public static function init(){
+        if (version_compare(phpversion(), '5.4.0','<')){
+            if (session_id() == ' '){
+                session_start();
+            }elseif(session_status() == PHP_SESSION_NONE){
+                session_start();
+            }
+        }
+    }
+    public static function set($key, $val){
+        $_SESSION[$key] = $val;
+    }
+    public static function get($key){
+        if(isset($_SESSION[$key])){
+            return $_SESSION[$key];
+        }else{
+            return false;
+        }
+    }
+    public static function checkLogin(){
+        self :: init();
+        if(self::get("login") == true){
+            header("Location:index.php");
+        }
+    }
+    public static destroy(){
+        session_destroy();
+        header("Location:index.php");
+    }
+}?>
