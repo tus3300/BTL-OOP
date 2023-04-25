08b6ef642e7cb353dabef847c3a9f2ee4a50c854
idea <21012098@st.phenikaa-uni.edu.vn>
2023-04-25 22:19:03 +0700

    up file

---
+++ V.Tu/admin/cartegoryadd.php
@@ -0,0 +1,10 @@
+        <div class="admin-content-right">
+            <div class="admin-content-right-carterogy">
+                <h1>Them danh muc</h1>
+                <form action="" method="POST">
+                    <input type="text" placeholder="Nhap ten">
+                    <button style="background-color:red;border:cyan;color:white";type="submit">Add</button>
+                </form>
+            </div>
+        </div>
+</section>
---
+++ V.Tu/admin/slider.php
@@ -0,0 +1,22 @@
+<section class="admin-content">
+        <div class="admin-content-left">
+            <ul>
+                <li><a href="#">Danh muc</a>
+                    <ul>
+                        <li><a href>Them danh muc</a></li>
+                        <li><a href>Danh sach danh muc</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Loai san pham</a>
+                    <ul>
+                        <li><a href>Them loai sp</a></li>
+                        <li><a href>Danh sach loai sp</a></li>
+                    </ul>
+                </li><li><a href="#">San pham</a>
+                    <ul>
+                        <li><a href>Them san pham</a></li>
+                        <li><a href>Danh sach sp</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
