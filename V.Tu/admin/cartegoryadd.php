11ac3ec1df6a2baba972fe4e0ae51e11b9508a35
idea <21012098@st.phenikaa-uni.edu.vn>
2023-05-03 09:27:03 +0700

    up file

--- V.Tu/admin/cartegoryadd.php
+++ V.Tu/admin/cartegoryadd.php
@@ -1,10 +1,22 @@
-        <div class="admin-content-right">
-            <div class="admin-content-right-carterogy">
+<?php
+include "header.php";
+include "slider.php";
+include "class/category-class.php";
+?> 
+<?php
+$cartegory
+?>    
+    
+<div class="admin-content-right">
+        <div class="admin-content-right-carterogy">
                 <h1>Them danh muc</h1>
                 <form action="" method="POST">
-                    <input type="text" placeholder="Nhap ten">
+                    <input name ="cartegory_name" type="text" placeholder="Nhap ten">
                     <button style="background-color:red;border:cyan;color:white";type="submit">Add</button>
                 </form>
             </div>
         </div>
-</section>
+    </section>
+
+</body>
+</html>
