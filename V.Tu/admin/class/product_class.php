622817c6d35e6b5a7a4e85f40ce1ccc65fb37a24
idea <21012098@st.phenikaa-uni.edu.vn>
2023-05-11 16:06:34 +0700

    up file

--- V.Tu/admin/class/product_class.php
+++ V.Tu/admin/class/product_class.php
@@ -18,7 +18,8 @@ class product{
         $product_desc = $_POST['product_desc'];
         $product_img = $_FILES['product_img']['name'];
         $filetype = strtolower(pathinfo($product_img,PATHINFO_EXTENSION));
-        if(file_exists("imgs / $filetarget ")){
+        $filesize = $_FILES['product_img']['size'];
+        if(file_exists("imgs/$filetarget")) {
             $alert = "file đã tồn tại";
             return $alert;
         }
@@ -29,36 +30,43 @@ class product{
             return $alert;
         }
         else{   
-        move_uploaded_file($_FILES['product_img']['name'], "imgs/".$_FILES['product_img']['name']);
-        $query = "INSERT INTO tbl_product(
-        product_name,
-        cartegory_id,
-        brand_id,
-        product_price,
-        product_price_saleoff,
-        product_desc,
-        product_img) VALUES 
-        ('$product_name',
-        '$cartegory_id',
-        '$brand_id',
-        '$product_price',
-        '$product_price_saleoff',
-        '$product_desc',
-        '$product_img')";
-        $result = $this->db->insert($query);
-        if($result){
-            $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1";
-            $result = $this->db->select($query)->fetch_assoc();
-            $product_id = $result['product_id'];
-            $filename = $_FILES['product_img_desc']['name'];
-            $filetmp = $_FILES['product_img_desc']['tmp_name'];
-            $filettarget = basename($_FILES['product_img_desc']['tmp_name']);
-
-            foreach ($filename as $key => $value){
-                move_uploaded_file( $filetmp [$key], "imgs/".$value);
-                $query ="INSERT INTO tbl_product_img_desc  (product_id,product_img_desc) VALUES ('$product_id','$value') ";
-                $result = $this->db->insert($query);
+            if($filesize > 1000000){
+                $alert = "file không được lớn hơn 1MB ";
+                return $alert;
             }
+            else{
+                move_uploaded_file($_FILES['product_img']['name'], "imgs/".$_FILES['product_img']['name']);
+                $query = "INSERT INTO tbl_product(
+                product_name,
+                cartegory_id,
+                brand_id,
+                product_price,
+                product_price_saleoff,
+                product_desc,
+                product_img) VALUES 
+                ('$product_name',
+                '$cartegory_id',
+                '$brand_id',
+                '$product_price',
+                '$product_price_saleoff',
+                '$product_desc',
+                '$product_img')";
+                $result = $this->db->insert($query);
+                if($result){
+                    $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1";
+                    $result = $this->db->select($query)->fetch_assoc();
+                    $product_id = $result['product_id'];
+                    $filename = $_FILES['product_img_desc']['name'];
+                    $filetmp = $_FILES['product_img_desc']['tmp_name'];
+                    $filettarget = basename($_FILES['product_img_desc']['tmp_name']);
+        
+                    foreach ($filename as $key => $value){
+                        move_uploaded_file( $filetmp [$key], "imgs/".$value);
+                        $query ="INSERT INTO tbl_product_img_desc  (product_id,product_img_desc) VALUES ('$product_id','$value') ";
+                        $result = $this->db->insert($query);
+                    }
+            }
+       
         }
         }
         }
